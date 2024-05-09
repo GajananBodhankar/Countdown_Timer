18db2d991df9b03f66907c46a7f3b568a689a758
GajananBodhankar <bodhankargajanan99@gmail.com>
2024-05-09 13:21:12 +0530

    Working on redux saga

--- src/Saga/Store.jsx
+++ src/Saga/Store.jsx
@@ -1,14 +1,36 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { SliceReducer } from "./slice";
+import { SliceReducer, failure, success } from "./slice";
 import createSagaMiddleware from "redux-saga";
 import watch from "./fork";
+import axios from "axios";
 const sagaMiddleware = createSagaMiddleware();
+// function customMiddleware(store) {
+//   return function (next) {
+//     return function (action) {
+//       console.log(action, store, next);
+//       apiCall(action, store);
+//       next(action);
+//     };
+//   };
+// }
 const Store = configureStore({
   reducer: {
     SliceReducer,
   },
-  middleware: (getdefault) => getdefault().concat(sagaMiddleware),
+  //   middleware: (getdefault) => getdefault().concat(customMiddleware),
+  middleware: (defMiddleware) => defMiddleware().concat(sagaMiddleware),
 });
 sagaMiddleware.run(watch);
+
+async function apiCall(action, store) {
+  try {
+    if (action.type.includes("pending")) {
+      let result = await axios.get("https://fakestoreapi.com/products");
+      store.dispatch(success(result.data));
+    }
+  } catch {
+    store.dispatch(failure());
+  }
+}
 
 export default Store;

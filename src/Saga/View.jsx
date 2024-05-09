abeed568c9969d3cbd8e4d2f74e0f01a0c77f335
GajananBodhankar <bodhankargajanan99@gmail.com>
2024-05-09 16:48:48 +0530

    made a change

--- src/Saga/View.jsx
+++ src/Saga/View.jsx
@@ -1,13 +1,16 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { pending } from "./slice";
 
 function View() {
   const { data, status } = useSelector((state) => state.SliceReducer);
+  const [d, s] = useState([]);
   const dispatch = useDispatch();
   useEffect(() => {
     console.log(data, status);
-  }, [status]);
+    dispatch(pending());
+  }, []);
+
   return (
     <div>
       hello
